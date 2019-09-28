c8450888e7de502b992f1521259e4400eebc87fa
Ivan Nikolsky <54115104+enty8080@users.noreply.github.com>
2019-09-28 22:55:23 +0300

    Update check.sh

--- etc/check.sh
+++ etc/check.sh
@@ -26,9 +26,9 @@ exit
 fi
 fi
 
-echo -e "Usage: ./check.sh [OPTIONS...]"
+echo -e "Usage: ./check.sh [OPTION...]"
 echo -e "Copyright (C) 2019, Entynetproject. All Rights Reserved."
-echo -e ""
+echo -e
 echo -e "  -i  Installation mode."
 echo -e "  -u  Uninstallation mode."
 exit
