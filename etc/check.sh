1de590805fedfaa3de58085ccb0c454286bb6187
Ivan Nikolsky <54115104+enty8080@users.noreply.github.com>
2019-09-28 22:53:45 +0300

    Create check.sh

---
+++ etc/check.sh
@@ -0,0 +1,34 @@
+#! /bin/bash
+
+if [[ $1 = "-i" ]]
+then
+if [[ -d ~/osfetch ]]
+then
+sleep 0
+exit
+else
+cd ~
+git clone https://github.com/entynetproject/osfetch
+cd ~/osfetch
+exit
+fi
+fi
+
+if [[ $1 = "-u" ]]
+then
+if [[ -d ~/osfetch ]]
+then
+rm -r ~/osfetch
+exit
+else
+sleep 0
+exit
+fi
+fi
+
+echo -e "Usage: ./check.sh [OPTIONS...]"
+echo -e ""
+echo -e ""
+echo -e "  -i  Installation mode."
+echo -e "  -u  Uninstallation mode."
+echo -e ""
